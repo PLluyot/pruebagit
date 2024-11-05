28b9a8b01d3b5a76c11fd8960cc08579a24c2fb9
Pepe Lluyot <pepe.lluyot@iescristobaldemonroy.es>
2024-11-05 19:43:53 +0100

    inicio

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+    //código php
+?>
+<!DOCTYPE html>
+<html lang='es'>
+<head>
+    <meta charset='UTF-8'>
+    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+    <title>P.Lluyot</title>
+    <link rel='stylesheet' href='https://cdn.simplecss.org/simple.min.css'>
+</head>
+<body>
+    <header><h2></h2></header>
+    <main>
+        <!-- código php -->
+        <?php
+        
+        ?>
+        <p class='notice'></p>
+    </main>
+    <footer><p>P.Lluyot</p></footer>
+</body>
+</html>
